5b6e4c88ebdd149fac21adbef0e308a2dcc31a31
sslyxhz <sslyxhz@hotmail.com>
2017-07-09 11:01:07 +0800

    Use NanoHTTPD to build simple http server

--- app/src/main/java/com/sslyxhz/androidserver/MainActivity.java
+++ app/src/main/java/com/sslyxhz/androidserver/MainActivity.java
@@ -2,12 +2,45 @@ package com.sslyxhz.androidserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String ROOT_DOCUMENT_PATH = "/html5up-dimension";
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webview_container);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+
+        startHttpServer();
+
+        final String url = "http://localhost:8080/";
+        mWebView.loadUrl(url);
+    }
+
+    private void startHttpServer() {
+        SimpleHttpServer server = new SimpleHttpServer(8080, ROOT_DOCUMENT_PATH, this);
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
